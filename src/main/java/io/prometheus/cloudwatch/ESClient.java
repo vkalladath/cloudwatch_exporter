73d3c4e1bf6d4b7d94b1805d56f4c34058e2d4e7
vkalladath <vkalladath@gmail.com>
2020-08-25 16:40:35 -0700

    Update ESClient.java

--- src/main/java/io/prometheus/cloudwatch/ESClient.java
+++ src/main/java/io/prometheus/cloudwatch/ESClient.java
@@ -34,7 +34,7 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class ESClient {
-	
+
     private static final Logger LOGGER = Logger.getLogger(ESClient.class.getName());
 
     private static final String UNTAGGED = "UNTAGGED";
@@ -51,25 +51,39 @@ public class ESClient {
 
     public static Map<String, String> findTagsForResource(String fieldName, String fieldValue, String esPath, List<String> additionalLabels) {
         Map<String, String> tags = defaultEmptyTags(TAG_NAMES, additionalLabels);
+        tags.put("Resource_type", WebServer.resourceType);
+        LOGGER.info(" Resource type is : " + WebServer.resourceType);
+        LOGGER.info("FieldName: " + fieldName + "   FieldValue: " + fieldValue);
         Unirest.setHttpClient(makeClient());
-
-        String sb = buildQuery(fieldName.toLowerCase(), fieldValue);
+        String updatedfieldValue = fieldValue;
+        String elasticache = "aws/elasticache";
+        if(esPath.equals(elasticache)) {
+              updatedfieldValue = RemoveNumbers(fieldValue);
+        }
+        String sb = buildQuery(fieldName.toLowerCase(), updatedfieldValue);
         JsonNode node;
         try {
-        	LOGGER.info("## Connecting to ES " + fieldValue + " " + fieldName);
+       //       LOGGER.info("## Connecting to ES " + fieldValue + " " + fieldName);
             node = Unirest.post("https://search.pacman.corporate.t-mobile.com/api/console/proxy?path=" + esPath.toLowerCase() + "/_search&method=POST").header("kbn-xsrf", "1")
                     .body(sb).asJson().getBody();
         } catch (UnirestException e) {
             // TODO Log Error
-        	LOGGER.warning("Error connecting to Pacman API: " + e.getMessage());
+                LOGGER.warning("Error connecting to Pacman API: " + e.getMessage());
+                LOGGER.info("Inside Catch due to error" + e.getMessage());
             return tags;
         }
         int totalResults = node.getObject().getJSONObject("hits").getInt("total");
-        LOGGER.fine("fieldName: " + fieldName + " fieldValue: " + fieldValue + " esPath: " +esPath);
+        if(fieldValue =="PRD-SOC-WEB-CUS"){
+        LOGGER.info("TotalResults=" + totalResults + "FieldName=" + fieldName + "FieldValue" + fieldValue + "updatedfieldvalue=" + updatedfieldValue);
+}
+        //LOGGER.fine("fieldName: " + fieldName + " fieldValue: " + fieldValue + " esPath: " +esPath);
         if (totalResults == 1) {
             JSONObject source = node.getObject().getJSONObject("hits").getJSONArray("hits").getJSONObject(0).getJSONObject("_source");
             // Tag names are case sensitive
-            
+
+            if(fieldValue == "PRD-SOC-WEB-CUS"){
+               LOGGER.info("inside prd");
+            }
             if(additionalLabels != null && additionalLabels.size() > 0) {
                 extractTags(tags, source, additionalLabels.toArray(new String[additionalLabels.size()]));
             }
@@ -79,21 +93,22 @@ public class ESClient {
         } else {
             // TODO: error
         }
+        //LOGGER.info(tags);
         return tags;
     }
 
-	private static HashMap<String, String> defaultEmptyTags(String[] tagNames, List<String> additionalLabels) {
-		HashMap<String, String> tags = new HashMap<String, String>();
-		for (String tagName : tagNames) {
-			tags.put(sanitizeTagName(tagName), UNTAGGED);
-		}
-		if(additionalLabels != null && additionalLabels.size() > 0) {
-			for (String tagName : additionalLabels) {
-				tags.put(sanitizeTagName(tagName), UNTAGGED);
-			}
-		}
-		return tags;
-	}
+    private static HashMap<String, String> defaultEmptyTags(String[] tagNames, List<String> additionalLabels) {
+        HashMap<String, String> tags = new HashMap<String, String>();
+        for (String tagName : tagNames) {
+            tags.put(sanitizeTagName(tagName), UNTAGGED);
+        }
+        if(additionalLabels != null && additionalLabels.size() > 0) {
+            for (String tagName : additionalLabels) {
+                tags.put(sanitizeTagName(tagName), UNTAGGED);
+            }
+        }
+        return tags;
+    }
 
     private static void extractTags(Map<String, String> tags, JSONObject source, String[] tagNames) {
         for (String tagName : tagNames) {
@@ -115,6 +130,20 @@ public class ESClient {
 
     private static String buildQuery(String fieldName, String fieldValue) {
         return "{\"size\":\"2\",\"query\":{\"bool\":{\"must\":[{\"term\":{\"" + fieldName + ".keyword\":\"" + fieldValue + "\"}},{\"match\":{\"latest\":true}}]}}}";
+    }
+
+    private static String RemoveNumbers(String fieldValue) {
+        while(fieldValue.matches("^.+?-\\d{1,5}$")) {
+          if (null != fieldValue && fieldValue.length() > 0 ) {
+              int endIndex = fieldValue.lastIndexOf("-");
+              if (endIndex != -1)
+              {
+                  String newstr = fieldValue.substring(0, endIndex);
+                  fieldValue = newstr;
+              }
+          }
+        }
+    return fieldValue;
     }
 
     public static HttpClient makeClient() {
